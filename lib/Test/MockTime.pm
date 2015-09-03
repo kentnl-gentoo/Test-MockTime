534a63498b855a03653f1117f296f18daa403424
DDICK <ddick@cpan.org>
2015-09-03 12:04:29 +0000

    Import https://cpan.metacpan.org/authors/id/D/DD/DDICK/Test-MockTime-0.14.tar.gz

--- lib/Test/MockTime.pm
+++ lib/Test/MockTime.pm
@@ -6,88 +6,97 @@ use Carp();
 use Exporter();
 *import = \&Exporter::import;
 our @EXPORT_OK = qw(
-    set_relative_time
-    set_absolute_time
-    set_fixed_time
-    restore_time
+  set_relative_time
+  set_absolute_time
+  set_fixed_time
+  restore_time
 );
-our %EXPORT_TAGS = (
-    'all' => \@EXPORT_OK,
-);
-our ($VERSION) = '0.13';
-our ($offset) = 0;
-our ($fixed) = undef;
+our %EXPORT_TAGS = ( 'all' => \@EXPORT_OK, );
+our $VERSION     = '0.14';
+our $offset      = 0;
+our $fixed       = undef;
 
 BEGIN {
-	*CORE::GLOBAL::time = \&Test::MockTime::time;
-	*CORE::GLOBAL::localtime = \&Test::MockTime::localtime;
-	*CORE::GLOBAL::gmtime = \&Test::MockTime::gmtime;
+    *CORE::GLOBAL::time      = \&Test::MockTime::time;
+    *CORE::GLOBAL::localtime = \&Test::MockTime::localtime;
+    *CORE::GLOBAL::gmtime    = \&Test::MockTime::gmtime;
 }
 
 sub set_relative_time {
-	my ($relative) = @_;
-	if (($relative eq __PACKAGE__) || (UNIVERSAL::isa($relative, __PACKAGE__))) {
-		Carp::carp("Test::MockTime::set_relative_time called incorrectly\n");
-	}
-	$offset = $_[-1]; # last argument. might have been called in a OO syntax?
+    my ($relative) = @_;
+    if (   ( $relative eq __PACKAGE__ )
+        || ( UNIVERSAL::isa( $relative, __PACKAGE__ ) ) )
+    {
+        Carp::carp("Test::MockTime::set_relative_time called incorrectly\n");
+    }
+    $offset = $_[-1];    # last argument. might have been called in a OO syntax?
+    return $offset;
 }
 
 sub _time {
-	my ($time, $spec) = @_;
-	unless ($time =~ /\A -? \d+ \z/xms) {
-		$spec ||= '%Y-%m-%dT%H:%M:%SZ';
-	}
-	if ($spec) {
-		require Time::Piece;
-		$time = Time::Piece->strptime($time, $spec)->epoch();
-	}
-	return $time;
+    my ( $time, $spec ) = @_;
+    if ( $time !~ /\A -? \d+ \z/xms ) {
+        $spec ||= '%Y-%m-%dT%H:%M:%SZ';
+    }
+    if ($spec) {
+        require Time::Piece;
+        $time = Time::Piece->strptime( $time, $spec )->epoch();
+    }
+    return $time;
 }
 
 sub set_absolute_time {
-	my ($time, $spec) = @_;
-	if (($time eq __PACKAGE__) || (UNIVERSAL::isa($time, __PACKAGE__))) {
-		Carp::carp("Test::MockTime::set_absolute_time called incorrectly\n");
-	}
-	$time = _time($time, $spec);
-	$offset = $time - CORE::time;
+    my ( $time, $spec ) = @_;
+    if ( ( $time eq __PACKAGE__ ) || ( UNIVERSAL::isa( $time, __PACKAGE__ ) ) )
+    {
+        Carp::carp("Test::MockTime::set_absolute_time called incorrectly\n");
+    }
+    $time = _time( $time, $spec );
+    $offset = $time - CORE::time;
+    return $offset;
 }
 
 sub set_fixed_time {
-	my ($time, $spec) = @_;
-	if (($time eq __PACKAGE__) || (UNIVERSAL::isa($time, __PACKAGE__))) {
-		Carp::carp("Test::MockTime::set_fixed_time called incorrectly\n");
-	}
-	$time = _time($time, $spec);
-	$fixed = $time;
+    my ( $time, $spec ) = @_;
+    if ( ( $time eq __PACKAGE__ ) || ( UNIVERSAL::isa( $time, __PACKAGE__ ) ) )
+    {
+        Carp::carp("Test::MockTime::set_fixed_time called incorrectly\n");
+    }
+    $time = _time( $time, $spec );
+    $fixed = $time;
+    return $fixed;
 }
 
-sub time() { 
-	if (defined $fixed) {
-		return $fixed;
-	} else {
-		return (CORE::time + $Test::MockTime::offset);
-	}
+sub time() {
+    if ( defined $fixed ) {
+        return $fixed;
+    }
+    else {
+        return ( CORE::time + $offset );
+    }
 }
 
 sub localtime (;$) {
-	my ($time) = @_;
-	unless (defined $time) {
-		$time = Test::MockTime::time();
-	}
-	return CORE::localtime($time);
+    my ($time) = @_;
+    if ( !defined $time ) {
+        $time = Test::MockTime::time();
+    }
+    return CORE::localtime($time);
 }
 
 sub gmtime (;$) {
-	my ($time) = @_;
-	unless (defined $time) {
-		$time = Test::MockTime::time();
-	}
-	return CORE::gmtime($time);;
+    my ($time) = @_;
+    if ( !defined $time ) {
+        $time = Test::MockTime::time();
+    }
+    return CORE::gmtime($time);
 }
 
 sub restore {
-	$offset = 0;
-	$fixed = undef;
+    $offset = 0;
+    $fixed  = undef;
+    return;
 }
 *restore_time = \&restore;
+
+1;
